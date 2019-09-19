81ba9cd53de18f617492b939255638bf438a56d2
zjiang333 <zjiang333@gatech.edu>
2019-09-19 02:48:22 -0400

    hw4

---
+++ 4-interacting-particles/hpctoolkit-cloud-database-107685.ice-sched.pace.gatech.edu-107685.ice-sched.pace.gatech.edu/src/nv/coc-ice/zjiang333/cse6230-hw/4-interacting-particles/verlet_stream_and_noise.c
@@ -0,0 +1,37 @@
+
+#include "cloud.h"
+#include "verlet.h"
+
+void
+verlet_step_stream_and_noise (int Np, double dt_stream, double dt_noise,
+                              cse6230rand_t *rand,
+                              double *restrict X[3], const double *restrict U[3])
+{
+  if (dt_noise) {
+    size_t tag;
+
+    tag = cse6230rand_get_tag (rand);
+      
+#pragma omp parallel for
+    for (int i = 0; i < Np; i++) {
+      double rval[4];
+
+      cse6230rand_hash (rand, tag, i, 0, 0, rval);
+      
+//       int my_thread = omp_get_thread_num();
+//       printf("iteration %d, thread %d\n", i, my_thread);
+        
+      for (int d = 0; d < 3; d++) {
+        X[d][i] += dt_stream * U[d][i] + dt_noise * (2. * rval[d] - 1.);
+      }
+    }
+  }
+  else {
+#pragma omp parallel for      
+    for (int i = 0; i < Np; i++) {
+      for (int d = 0; d < 3; d++) {
+        X[d][i] += dt_stream * U[d][i];
+      }
+    }
+  }
+}
